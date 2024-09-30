87c3d50bdb9f4e24b8a95044ca210eb164a829c3
consignr <matt.ost17@googlemail.com>
2024-09-30 22:39:26 +0100

    Remove Create Computer action from list resource

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # filament-print-node
  A collection of filament resources to manage your printnode account from the admin panel
+ 
--- src/Clusters/PrintNode/Resources/ComputerResource/Pages/ListComputers.php
+++ src/Clusters/PrintNode/Resources/ComputerResource/Pages/ListComputers.php
@@ -3,7 +3,6 @@
 namespace Consignr\FilamentPrintNode\Clusters\PrintNode\Resources\ComputerResource\Pages;
 
 use Consignr\FilamentPrintNode\Clusters\PrintNode\Resources\ComputerResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListComputers extends ListRecords
@@ -13,7 +12,7 @@ class ListComputers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //
         ];
     }
 }
