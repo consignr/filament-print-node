24c4ad569d2681bd6e6201c4f4d2db9b1ef23dde
consignr <matt.ost17@googlemail.com>
2024-10-05 00:32:21 +0100

    Configure PrinterResource.php and Printer.php model
    Setup an infolist for the view action in  a slideover. Create accessors for the capability attributes.

--- src/Clusters/PrintNode/Resources/PrinterResource.php
+++ src/Clusters/PrintNode/Resources/PrinterResource.php
@@ -5,21 +5,25 @@ namespace Consignr\FilamentPrintNode\Clusters\PrintNode\Resources;
 use Filament\Forms;
 use Filament\Tables;
 use Filament\Forms\Form;
-
 use Filament\Tables\Table;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Illuminate\Support\HtmlString;
 use Illuminate\Support\Facades\Blade;
-use Filament\Tables\Columns\IconColumn;
+use Filament\Infolists\Components\Tabs;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\SelectColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\Fieldset;
+use Filament\Infolists\Components\Tabs\Tab;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\TextEntry;
 use Consignr\FilamentPrintNode\Models\Printer;
 use Consignr\FilamentPrintNode\Clusters\PrintNode;
 use Consignr\FilamentPrintNode\Enums\PrinterState;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Consignr\FilamentPrintNode\Clusters\PrintNode\Resources\PrinterResource\Pages;
 use Consignr\FilamentPrintNode\Clusters\PrintNode\Resources\PrinterResource\RelationManagers;
+use Filament\Support\Enums\MaxWidth;
 
 class PrinterResource extends Resource
 {
@@ -35,6 +39,72 @@ class PrinterResource extends Resource
             ->schema([
                 //
             ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Tabs::make('Capabilities')
+                    ->contained(false)
+                    ->columnSpanFull()
+                    ->schema([
+                        Tab::make('General')
+                            ->columns(2)
+                            ->schema([
+                                TextEntry::make('tray_names')->listWithLineBreaks()->separator(',')->placeholder('-'),
+                                TextEntry::make('supported_dpis')->listWithLineBreaks()->label('Supported DPI\'s'),
+                                IconEntry::make('collate')->boolean()->label('Collation Support'),
+                                TextEntry::make('copies')->label('Maximum copies'),
+                                IconEntry::make('color')->boolean()->label('Colour Support'),                                
+                                IconEntry::make('duplex')->boolean()->label('Duplex Support'),                
+                                TextEntry::make('n_up_printing')->listWithLineBreaks()->separator(',')->placeholder('-'), 
+                                TextEntry::make('printrate')
+                                    ->formatStateUsing(fn (?array $state): float => $state ? $state['rate'] : null)
+                                    ->suffix(fn (?array $state): float => $state ? $state['unit'] : null)
+                                    ->placeholder('-'),
+                                IconEntry::make('supports_custom_paper_size')->boolean(),
+                                Fieldset::make('SupportedWidth')
+                                    ->schema([
+                                        TextEntry::make('minimum_supported_width')->formatStateUsing(fn (int $state): int => $state/10)->suffix('mm')->label('Minimum'),
+                                        TextEntry::make('maximum_supported_width')->formatStateUsing(fn (int $state): int => $state/10)->suffix('mm')->label('Maximum'),
+                                    ]),
+                                Fieldset::make('SupportedHeight')
+                                    ->schema([
+                                        TextEntry::make('minimum_supported_height')->formatStateUsing(fn (int $state): int => $state/10)->suffix('mm')->label('Minimum'),                                
+                                        TextEntry::make('maximum_supported_height')->formatStateUsing(fn (int $state): int => $state/10)->suffix('mm')->label('Maximum'),
+                                    ]),
+                            ]),
+                        Tab::make('Media')
+                            ->schema([
+                                TextEntry::make('media_names')->listWithLineBreaks()->separator(',')->placeholder('-')
+                            ]),
+                        Tab::make('Papers')
+                            ->schema(function ($record) {
+                                $papers = [];
+                                foreach ($record->papers as $key => $paper) {
+                                    $papers[] = Section::make(null)
+                                        ->description($key)
+                                        ->collapsible()
+                                        ->collapsed()
+                                        ->compact()
+                                        ->columns(2)
+                                        ->schema([
+                                            TextEntry::make('width')
+                                                ->getStateUsing(fn () => $paper['width'])
+                                                ->formatStateUsing(fn (int $state): int => $state/10)
+                                                ->suffix('mm'),
+                                            TextEntry::make('height')
+                                                ->getStateUsing(fn () => $paper['height'])
+                                                ->formatStateUsing(fn (int $state): int => $state/10)
+                                                ->suffix('mm')
+                                        ]);
+                                }
+
+                                return $papers;
+                            }),
+                    ])
+             ]);
     }
 
     public static function table(Table $table): Table
@@ -66,7 +136,12 @@ class PrinterResource extends Resource
                 //
             ])
             ->actions([
-                //
+                Tables\Actions\ViewAction::make()
+                    ->label('Capabilities')
+                    ->icon('heroicon-o-information-circle')
+                    ->slideOver()
+                    ->modalHeading('Printer Capabilities')
+                    ->modalWidth(MaxWidth::Large)
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- src/Models/Printer.php
+++ src/Models/Printer.php
@@ -2,19 +2,185 @@
 
 namespace Consignr\FilamentPrintNode\Models;
 
-use Consignr\FilamentPrintNode\Enums\PrinterState;
 use Sushi\Sushi;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Consignr\FilamentPrintNode\Enums\PrinterState;
 
 class Printer extends Model
 {
     use Sushi;
 
     protected $casts = [
-        'state' => PrinterState::class
+        'state' => PrinterState::class,
+        'capabilities' => 'array'
     ];
+
+    /**
+     * Get the capabilities.
+     */
+    protected function capabilities(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value): array => json_decode($value, true),
+        );
+    }
+
+    /**
+     * Get the tray_names.
+     */
+    protected function trayNames(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): array => $this->capabilities['bins'],
+        );
+    }
+
+    /**
+     * Get the collate.
+     */
+    protected function collate(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): bool => $this->capabilities['collate'],
+        );
+    }
+
+    /**
+     * Get the color.
+     */
+    protected function color(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): bool => $this->capabilities['color'],
+        );
+    }
+
+    /**
+     * Get the copies.
+     */
+    protected function copies(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): int => $this->capabilities['copies'],
+        );
+    }
+
+    /**
+     * Get the supported_dpis.
+     */
+    protected function supportedDpis(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): array => $this->capabilities['dpis'],
+        );
+    }
+
+    /**
+     * Get the duplex.
+     */
+    protected function duplex(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): bool => $this->capabilities['duplex'],
+        );
+    }
+
+    /**
+     * Get the media_names.
+     */
+    protected function mediaNames(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): array => $this->capabilities['medias'],
+        );
+    }
+
+    /**
+     * Get the n_up_printing.
+     */
+    protected function NUpPrinting(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): array => $this->capabilities['nup'],
+        );
+    }
+
+    /**
+     * Get the minimum_supported_width.
+     */
+    protected function minimumSupportedWidth(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): int => $this->capabilities['extent'][0][0],
+        );
+    }
+
+    /**
+     * Get the minimum_supported_height.
+     */
+    protected function minimumSupportedHeight(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): int => $this->capabilities['extent'][0][1],
+        );
+    }
+
+    /**
+     * Get the maximum_supported_width.
+     */
+    protected function maximumSupportedWidth(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): int => $this->capabilities['extent'][1][0],
+        );
+    }
+
+    /**
+     * Get the maximum_supported_height.
+     */
+    protected function maximumSupportedHeight(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): int => $this->capabilities['extent'][1][1],
+        );
+    }
+
+    /**
+     * Get the papers.
+     */
+    protected function papers(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): array => collect($this->capabilities['papers'])
+                ->map(function ($item) {
+                    $keys = ['width', 'height'];
+                    return array_combine($keys, $item);
+                })
+                ->toArray(),
+        );
+    }
+
+    /**
+     * Get the printrate.
+     */
+    protected function printrate(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): ?array => $this->capabilities['printrate'],
+        );
+    }
+
+    /**
+     * Get the supports_custom_paper_size.
+     */
+    protected function supportsCustomPaperSize(): Attribute
+    {
+        return Attribute::make(
+            get: fn (): bool => $this->capabilities['supports_custom_paper_size'],
+        );
+    }
     
     /**
      * Model Rows
@@ -26,21 +192,27 @@ class Printer extends Model
         //API
         $printers = Http::withBasicAuth(env('PRINTNODE_API_KEY'), env('PRINTNODE_PASSWORD'))
                          ->get('https://api.printnode.com/printers')->json();
-
+       
         //filtering some attributes
-        $printers = Arr::map($printers, function ($item) {
-            return Arr::only($item,
-                [
-                    "id",
-                    "name",
-                    "description",
-                    "default",
-                    "createTimestamp",
-                    "state"
-                ]
-            );
-        });
- 
+        $printers = collect($printers)->map(function ($item, $key) {
+            return collect($item)->map(function ($i, $k) {
+                
+                if ($k === 'capabilities') {
+                   return json_encode($i);
+                }
+
+                return $i;
+            })->only([
+                "id",
+                "name",
+                "description",
+                "default",
+                "createTimestamp",
+                "state",
+                "capabilities"
+            ]);
+        })->toArray();
+
         return $printers;
     }
 }
