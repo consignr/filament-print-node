2163ed4e22e6b71296d871800f7b264fca56ec74
consignr <matt.ost17@googlemail.com>
2024-10-05 21:41:39 +0100

    Update ViewComputer.php to include computer name in title and revise subHeading

--- src/Clusters/PrintNode/Resources/ComputerResource/Pages/ViewComputer.php
+++ src/Clusters/PrintNode/Resources/ComputerResource/Pages/ViewComputer.php
@@ -5,8 +5,19 @@ namespace Consignr\FilamentPrintNode\Clusters\PrintNode\Resources\ComputerResour
 use Consignr\FilamentPrintNode\Clusters\PrintNode\Resources\ComputerResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ViewComputer extends ViewRecord
 {
     protected static string $resource = ComputerResource::class;
+
+    public function getTitle(): string|Htmlable
+    {
+        return "{$this->record->name}";
+    }
+
+    public function getSubheading(): string|Htmlable|null
+    {
+       return 'View Computer'; 
+    }
 }
